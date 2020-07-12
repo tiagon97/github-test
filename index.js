11bd0a20935addcc4a029711821266bb6e0117ca
tiagon <radek.wojciechowski1997@gmail.com>
2020-07-12 13:39:17 +0200

    added some modulo func

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-
 const numbers = [1, 2, 3, 4, 5, 2, 3, 53, 21, 2, 1];
 
 const uniqueNumbers = new Set(numbers);
 console.log("heloo world");
 
+uniqueNumbers.forEach((num) => (num % 2 ? num * 2 : num * 10));
